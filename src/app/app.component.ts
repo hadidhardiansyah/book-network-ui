d4594ea26be7074025130fa55e7be62211c8547f
Hadid Hardiansyah <hadidhardiansyah@gmail.com>
2025-02-05 10:20:19 +0700

    Generate the HTTP Services using the power of OpenApi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'book-network-ui';
+  private http = inject(HttpClient);
+
+  constructor() {
+    this.http.get('https://api.example.com/data').subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
