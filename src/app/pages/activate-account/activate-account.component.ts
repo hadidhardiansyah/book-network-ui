09287664306ae1699831a3ec6d61052021e68827
Hadid Hardiansyah <hadidhardiansyah@gmail.com>
2025-02-06 13:42:20 +0700

    Implement the Activate account page

---
+++ src/app/pages/activate-account/activate-account.component.ts
@@ -0,0 +1,54 @@
+import { AuthenticationService } from '@/app/services/services';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CodeInputModule } from 'angular-code-input';
+
+@Component({
+  selector: 'app-activate-account',
+  standalone: true,
+  imports: [CommonModule, CodeInputModule],
+  templateUrl: './activate-account.component.html',
+  styleUrl: './activate-account.component.scss',
+})
+export class ActivateAccountComponent {
+  message: string = '';
+  isOkay: boolean = true;
+  submitted: boolean = false;
+
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {}
+
+  private confirmAccount(token: string) {
+    this.authService
+      .confirm({
+        token,
+      })
+      .subscribe({
+        next: () => {
+          this.message =
+            'Your account has been successfully activated.\nNow you can proceed to login';
+          this.submitted = true;
+          this.isOkay = true;
+          console.log('hadid');
+        },
+        error: (err) => {
+          console.log('gembil');
+
+          this.message = 'Token has been expired or invalid';
+          this.submitted = true;
+          this.isOkay = false;
+        },
+      });
+  }
+
+  onCodeCompleted(token: string) {
+    this.confirmAccount(token);
+  }
+
+  redirectToLogin() {
+    this.router.navigate(['login']);
+  }
+}
